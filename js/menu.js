8d9b91bd82a95ca3d449c2c828afcb6632b57206
Felipe <felilerner@gmail.com>
2020-12-22 20:54:01 -0300

    Commit Inicial

---
+++ js/menu.js
@@ -0,0 +1,44 @@
+
+    posicionarMenu();
+     
+    $(window).scroll(function() {    
+        posicionarMenu();
+    });
+     
+    function posicionarMenu() {
+        var altura_del_home = $('.home').outerHeight(true);
+        var altura_del_menu = $('.menu').outerHeight(true);
+        var mq = window.matchMedia( "(max-width: 570px)" );
+
+        if (mq.matches) {
+            $('.menu').addClass('fixed');
+            $('.wrapper').css('margin-top', (altura_del_menu) + 'px');
+            $('.nav_menu').addClass('d-none');
+            $('.menu').addClass('d-flex');
+        }
+        else {
+            if ($(window).scrollTop() >= altura_del_home - 80){
+                $('.menu').addClass('fixed');
+                $('.wrapper').css('margin-top', (altura_del_menu) + 'px');
+                
+                $('.menu').addClass('d-flex');
+            }
+            else {
+                $('.menu').removeClass('fixed');
+                $('.wrapper').css('margin-top', '0');
+                $('.nav_menu').removeClass('d-none');
+                $('.menu').removeClass('d-flex');
+            }   
+        }
+        if ($(window).scrollTop() >= altura_del_home - 100){
+            $('.nav_menu').addClass('d-none');
+        }
+
+        
+
+    }
+
+
+
+      
+
